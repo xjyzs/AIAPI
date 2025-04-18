46f728e6ad5e94cda88fb488128b61ea665c933d
xjyzs <158408146+xjyzs@users.noreply.github.com>
2025-04-19 00:58:56 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.xjyzs.aiapi"
-        minSdk = 21
+        minSdk = 24
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
